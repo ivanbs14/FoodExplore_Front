6e31b4fc3293dd0d49d1e03ed3cb9b685dffdb83
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-03-21 15:19:48 -0300

    user registration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,9 @@
 
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
 import { Conteiner, Form } from "./styles";
+
+import { api } from "../../services/api";
 
 import BackgroundImg from '../../assets/LogFoodExplorer.png';
 
@@ -9,6 +12,30 @@ import { Button } from '../../components/Button';
 import { Section } from '../../components/Section'; 
 
 export function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!");
+        }
+
+        api.post("/users", { name, email, password })
+            .then(() => {
+                alert("Usuário cadastrado com sucesso!");
+                navigate("/");
+            })
+            .catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message);
+                } else {
+                    alert("Não foi possível cadastrar");
+                }
+            });
+    }
 
     return (
         <Conteiner>
@@ -21,6 +48,7 @@ export function SignUp() {
                     <Input 
                         placeholder="Exemplo: Maria da Silva"
                         type="text"
+                        onChange={e => setName(e.target.value)}
                     />
                 </Section>
 
@@ -28,6 +56,7 @@ export function SignUp() {
                     <Input 
                         placeholder="Exemplo: exemplo@exemplo.com.br"
                         type="text"
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </Section>
                 
@@ -35,10 +64,11 @@ export function SignUp() {
                     <Input 
                         placeholder="No mínimo 6 caracteres"
                         type="text"
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </Section>
 
-                <Button title="Criar conta"/>
+                <Button title="Criar conta" onClick={handleSignUp}/>
 
                 <Link to="/">Já tenho uma conta</Link>
             </Form>
